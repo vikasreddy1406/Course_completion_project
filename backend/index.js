49d6e25e3f03d878b466c5da03ea0aeca3c39d1f
vikasreddy1406 <vikassai345@gmail.com>
2024-09-30 13:09:35 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import "dotenv/config.js"
+import express from "express"
+import cors from "cors"
+import connectToDB from "./config/db.js"
+import cookieParser from "cookie-parser"
+
+connectToDB()
+const app=express()
+
+app.use(cors({
+    origin: "*",
+    credentials: true
+}))
+app.use(express.json())
+app.use(cookieParser())
+const PORT=process.env.PORT
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server running on PORT ${PORT}`)
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import "./App.css"
+
+
+function App() {
+
+  return (
+    <>
+       <h1 className="text-3xl font-bold underline text-center">
+    Hello world! ihdsudgweg
+  </h1>
+    </>
+  );
+}
+
+export default function Root() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
