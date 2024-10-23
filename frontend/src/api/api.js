6c2c99e006afedbab61884e4084178dde3017174
vikasreddy1406 <vikassai345@gmail.com>
2024-10-23 12:10:41 +0530

    check prod

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000", 
+    baseURL: process.env.REACT_APP_API_URL, 
   });
 
-console.log('API Base URL:', api.defaults.baseURL);
+
   
 export default api;
